76b42f994a922429bc81d07d309185b750ef5351
shin039 <life.syncopation.4003@gmail.com>
2023-03-31 09:25:58 +0900

    フォーマットの調整

--- src/__tests__/basic.test.js
+++ src/__tests__/basic.test.js
@@ -9,10 +9,10 @@ import {sum} from 'src/basic/basic';
 // Basic Test Case
 // -----------------------------------------------------------------------------
 describe("■ テストケース", () => {
-  test        ('add 1 + 2 to equal 3'    , () => expect(sum(1, 2))    .toBe(3) )
-  it          ('it is alias of test'     , () => expect(sum(1, 2))    .toBe(3) )
+  test        ('add 1 + 2 to equal 3'    , () => expect(sum(1, 2)).toBe(3)     )
+  it          ('it is alias of test'     , () => expect(sum(1, 2)).toBe(3)     )
   test        ('add 1 + 2 to not equal 4', () => expect(sum(1, 2)).not.toBe(4) )
-  test.failing('add 1 + 2 to not equal 5', () => expect(sum(1, 2))    .toBe(5) )
+  test.failing('add 1 + 2 to not equal 5', () => expect(sum(1, 2)).toBe(5)     )
 
   test('resolve toBe'   , () => expect(Promise.resolve('resolved')           ).resolves.toBe   ('resolved') )
   test('reject  toThrow', () => expect(Promise.reject(new Error('rejected')) ).rejects .toThrow('rejected') )
